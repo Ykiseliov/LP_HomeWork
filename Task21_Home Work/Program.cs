e2b3cb98173bd5e5db7f0e30d2f8f9615d6a45f0
Ykiseliov <ykiseliov@gmail.com>
2023-05-02 20:57:00 +0300

    upd.task

--- Task21_Home Work/Program.cs
+++ Task21_Home Work/Program.cs
@@ -3,7 +3,7 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Координаты точки 1:");
+Console.WriteLine("A:");
             Console.Write("x: ");
             double x1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("y: ");
@@ -11,7 +11,7 @@ Console.WriteLine("Координаты точки 1:");
             Console.Write("z: ");
             double z1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Координаты точки 2:");
+            Console.WriteLine("B:");
             Console.Write("x: ");
             double x2 = Convert.ToInt32(Console.ReadLine());
             Console.Write("y: ");
@@ -21,4 +21,4 @@ Console.WriteLine("Координаты точки 1:");
 
             double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
 
-            Console.WriteLine($"Расстояние между 1 и 2 точками: {distance:F2}");
+            Console.WriteLine($"A({x1},{y1},{z1}); B({x2},{y2},{z2}): {distance:F2}");
