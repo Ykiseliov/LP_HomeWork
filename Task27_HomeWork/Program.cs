d21c63b957f4825f17d44718e917034aa191f371
Ykiseliov <ykiseliov@gmail.com>
2023-05-21 13:19:31 +0300

    add Task27 Условие и Решение

---
+++ Task27_HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 27: Напишите программу, которая принимает 
+//	на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = SumOfDigits(number);
+
+Console.WriteLine($"{number} -> {sum}");
+
+    /*static*/ int SumOfDigits(int number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
