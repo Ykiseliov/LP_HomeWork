0daa657754edd38ab1fdd7a3a98305cc771bbc31
Ykiseliov <ykiseliov@gmail.com>
2023-05-28 21:37:08 +0300

    add decision

--- Task34_HomeWork/Program.cs
+++ Task34_HomeWork/Program.cs
@@ -2,3 +2,29 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+
+int[] arr = new int[4];  //указываем кол-во чисел
+Random rnd = new Random(); // Создаем массив из случайных трехзначных целых чисел
+for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+
+// вызываем метод 
+int count = CountEvenNumbers(arr);
+ Console.Write($"[{string.Join(", ", arr)}]-> {count}");       
+
+// Метод для подсчета кол-ва четных чисел в массиве
+static int CountEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 == 0) // Проверяем четное ли число
+            {
+                count++; 
+            }
+        }
+        return count;
+}
