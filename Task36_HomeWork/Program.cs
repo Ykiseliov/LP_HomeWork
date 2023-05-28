9b62d37a92f9ae614c2aff196f723accd8925416
Ykiseliov <ykiseliov@gmail.com>
2023-05-28 21:41:43 +0300

    add decision

--- Task36_HomeWork/Program.cs
+++ Task36_HomeWork/Program.cs
@@ -4,3 +4,25 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] arr = new int[4];
+Random randNum = new Random(); // генерируем массив из  случайных чисел от 0 до 99
+for (int i = 0; i < 4; i++) 
+    {
+        arr[i] = randNum.Next(100);
+    }
+        
+//вызываем метод
+int sum = GetSumOnOddPositions(arr);
+
+Console.Write($"[{string.Join(", ", arr)}]-> {sum}");
+
+// находим сумму элементов, стоящих на нечётных позициях
+static int GetSumOnOddPositions(int[] arr) 
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2) 
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
