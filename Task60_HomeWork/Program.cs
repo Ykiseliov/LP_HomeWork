4277f4c96020327f2d2fe63269c501f8440f259b
Ykiseliov <ykiseliov@gmail.com>
2023-06-07 21:52:19 +0300

    add decision

--- Task60_HomeWork/Program.cs
+++ Task60_HomeWork/Program.cs
@@ -5,3 +5,33 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+ Console.Clear();
+        int[,,] arr = new int[2, 2, 2]
+        {
+            {
+                { 66, 25 },
+                { 34, 41 }
+            },
+            {
+                { 27, 90 },
+                { 26, 55 }
+            }
+        };
+
+        PrintArray(arr);
+
+    static void PrintArray(int[,,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                for (int k = 0; k < arr.GetLength(2); k++)
+                {
+                    Console.Write($"{arr[i,j,k]:00}({i},{j},{k}) ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
