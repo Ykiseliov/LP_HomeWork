4646ff8828322cfd5d7f2f3a576f9f6a88b650a5
Ykiseliov <ykiseliov@gmail.com>
2023-06-07 19:58:15 +0300

    Решение

--- Task56_HomeWork/Program.cs
+++ Task56_HomeWork/Program.cs
@@ -9,3 +9,48 @@
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер 
 // строки с наименьшей суммой элементов: 1 строка
+
+int[,] arr = new int[,] { { 1, 4, 7, 2 },{ 5, 9, 2, 3 }, { 8, 4, 2, 4 }, { 5, 2, 6, 7 } };
+
+PrintArray(arr);
+
+int minSumRow = FindMinSumRow(arr);
+            
+Console.WriteLine($"Cтрока с наименьшей суммой элементов: {minSumRow + 1}");
+
+int FindMinSumRow(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+
+    int minSum = int.MaxValue;
+    int minSumRow = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+        int rowSum = 0;
+            for (int j = 0; j < columns; j++)
+            {
+                rowSum += arr[i, j];
+            }
+            if (rowSum < minSum)
+            {
+            minSum = rowSum;
+            minSumRow = i;
+            }
+    }
+    return minSumRow;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+        Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
