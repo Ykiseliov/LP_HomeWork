1a30ecf03c257f0d01e3769175613f5f6d5e3679
Ykiseliov <ykiseliov@gmail.com>
2023-06-12 08:52:03 +0300

    add decision

--- Task68_HomeWork/Program.cs
+++ Task68_HomeWork/Program.cs
@@ -2,3 +2,27 @@
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число(M): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число(N): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = AckerFunc(m, n);
+Console.WriteLine($"m={m},n={n} -> A({m},{n}) = {result}");
+
+int AckerFunc(int m, int n)
+{
+    if (m == 0)
+        {
+            return n + 1;
+        }
+    if (n == 0)
+        {
+            return AckerFunc(m - 1, 1);
+        }
+    else
+        {
+            return AckerFunc(m - 1, AckerFunc(m, n - 1));
+        }
+}
